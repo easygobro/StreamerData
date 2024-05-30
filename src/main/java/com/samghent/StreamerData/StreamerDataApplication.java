9d441c04658eecc0f9feca4841cad9a9108b0c30
easy <sghent@oswego.edu>
2024-05-30 18:20:43 -0400

    first commit

---
+++ src/main/java/com/samghent/StreamerData/StreamerDataApplication.java
@@ -0,0 +1,13 @@
+package com.samghent.StreamerData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamerDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamerDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samghent/StreamerData/StreamerDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samghent.StreamerData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamerDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
